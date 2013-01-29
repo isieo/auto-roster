983f73efcad850181ae9783fea7c832122b46442
Calvin Tee <calvin@calvin-desktop.(none)>
2013-01-29 16:15:37 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoRoster::Application.config.secret_token = 'd373f3e1584ea00c28b5ecf0988dea15ce10ca899b3e82a5753d2cf4098080834aa65146d227a29bc7bfd42a1a0c9a970091aac617a7d19c7b334e31430e271d'
